b131d71a5b81763f96f942626b8d09dc77515a16
Jérémie Mazard <mazardjeremie@gmail.com>
2022-03-04 11:11:12 +0100

    IndexController setup & twig templating

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(): Response
+    {
+        return $this->render('index/index.html.twig');
+    }
+
+    #[Route('/signup', name: 'signup')]
+    public function signup(): Response
+    {
+        return $this->render('index/signup.html.twig');
+    }
+}
